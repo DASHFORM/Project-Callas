c5249a91d353975ecdca1cfa51fd8b78c226d97f
dashformdev <34486762+dashformdev@users.noreply.github.com>
2017-12-12 19:11:11 +0000

    Add files via upload

---
+++ sku page 1.php
@@ -0,0 +1,57 @@
+<div class="sku">
+<div class="container">
+  <!-- SKU Section - 1 row 6 columns-->
+  <div class="row">
+
+  <div class="col-md-2">
+<center> <img width="144" height="360" class="header-image" src="https://drogo-london.com/wp-content/uploads/2017/09/Layer-13.png"> </center>
+
+</div>
+
+
+<div class="col-md-2">
+<a href="https://drogo-london.com/shop/ORIGINAL"> <center> <h3>  DROGO </h3> </center> </a>
+<a href="https://drogo-london.com/shop/ORIGINAL"> <center> <p>  ORIGINAL  </p></a> </center>
+<p> Specially formulated using A grade cold-pressed oils, DROGO ORIGINAL provides the best care for your hair and skin. Through the use of carefully selected ingredients, our product helps to moisturise, condition, cleanse and soften your beard. Unscented with 100% natural ingredients, our beard oil is ideal for sensitive skin. Layer with your favourite fragrance for extra oomph.</p>
+<img width="87" height="16" class="header-image" src="https://drogo-london.com/wp-content/uploads/2017/09/star-star-star-star-star-copy-5.png">
+</div>
+
+
+<div class="col-md-2">
+ <center> <img width="144" height="360" class="header-image" src="https://drogo-london.com/wp-content/uploads/2017/09/Layer-16.png"> </center>
+
+
+
+</div>
+
+
+<div class="col-md-2">
+<a href="https://drogo-london.com/shop/ENDURE" > <center> <h3> DROGO </h3> </center> </a>
+<a href="https://drogo-london.com/shop/ENDURE">  <center> <p> ENDURE </p> </center>  </a>
+<p> DROGO ENDURE starts with a DROGO ORIGINAL base with the added benefits of high quality essential oils to create an unforgettable fresh, woody scent. Notes of Patchouli, Frankincense, Bergamot, Sandalwood, Cedar Wood and Vetiver provide a long-lasting scent rivalled only by luxury colognes. Our ENDURE oil blend promotes hair growth and helps to prevent damage of the hair follicle. </p>
+<img width="87" height="16" class="header-image" src="https://drogo-london.com/wp-content/uploads/2017/09/star-star-star-star-star-copy-5.png">
+
+</div>
+
+
+
+
+<div class="col-md-2">
+<center> <img width="144" height="360" class="header-image" src="https://drogo-london.com/wp-content/uploads/2017/09/Layer-15-copy.png"> </center>
+
+</div>
+
+
+<div class="col-md-2">
+<a href="https://drogo-london.com/shop/INFLUENCE"> <center> <h3> DROGO </h3> </center> </a>
+<a href="https://drogo-london.com/shop/INFLUENCE"><center> <p> INFLUENCE</p></center> </a>
+<p>DROGO INFLUENCE inherits the same conditioning formulation as DROGO ORIGINAL with the added benefits of high quality essential oils to create an energising citrus scent. Notes of pure Neroli, Bergamot and Sandalwood provide a fresh, invigorating scent that lasts all day long. Our specially selected oil blend tames frizzy hair, calms irritated skin and keeps your beard in top condition.  </p>
+<img width="87" height="16" class="header-image" src="https://drogo-london.com/wp-content/uploads/2017/09/star-star-star-star-star-copy-5.png">
+
+</div>
+
+
+      </div>
+
+
+    </div>
