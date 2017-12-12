d05efd24407f326b0bb321a26b35cc9fd07c00c1
dashformdev <34486762+dashformdev@users.noreply.github.com>
2017-12-12 18:24:55 +0000

    Add files via upload
    Code for Front Page - show by concern row.

---
+++ SBC 4 Images.php
@@ -0,0 +1,145 @@
+
+    <div class="touch">
+<h4 class="divider"><span> SHOP BY CONCERN </span></h4></div>
+
+
+<div class="blognormal">
+    <div class="container">
+      <!-- 2nd Row - 3 Columns-->
+      <div class="row">
+
+        <div class="col-md-3">
+        <a href="https://drogo-london.com/shop/ORIGINAL">
+
+
+          <center> <div class="containerBox">
+    <div class="text-box">
+        <h4></h4>
+    </div>
+    <img class="img-responsive" width="300" height="300" src="https://drogo-london.com/wp-content/uploads/2017/12/Unknown.png">
+</div> </center>
+
+        </div> </a>
+
+
+
+        <div class="col-md-3">
+        <a href="https://drogo-london.com/shop/ENDURE">
+
+
+          <center>
+
+            <div class="containerBox">
+      <div class="text-box">
+          <h4></h4>
+      </div>
+   <img class="img-responsive" width="300" height="300" src="https://drogo-london.com/wp-content/uploads/2017/12/Unknown-1.png">
+  </div></center>
+
+        </div> </a>
+
+
+        <div class="col-md-3">
+        <a href="https://drogo-london.com/shop/INFLUENCE">
+
+
+          <center>
+
+            <div class="containerBox">
+      <div class="text-box">
+          <h4></h4>
+      </div>
+   <img class="img-responsive" width="300" height="300" src="https://drogo-london.com/wp-content/uploads/2017/12/Unknown-2.png">
+  </div></center>
+
+        </div> </a>
+
+        <div class="col-md-3">
+        <a href="https://drogo-london.com/shop/OUD">
+
+
+<center>
+<div class="containerBox">
+      <div class="text-box">
+      <h4></h4>
+      </div>
+
+<img width="300" height="300" class="header-image" src="https://drogo-london.com/wp-content/uploads/2017/12/Unknown-3.png"> </div></center>
+
+
+        </div> </a>
+        </div>
+      </div>
+
+</div>
+
+
+
+<div class="blogmobile">
+    <div class="container">
+      <!-- 2nd Row - 3 Columns-->
+      <div class="row">
+        <div class="col-md-4">
+          <a href="https://drogo-london.com/shop/ORIGINAL">
+
+          <center>
+
+            <div class="containerBox">
+            <div class="text-box-mobile">
+            <h4></h4>
+            </div>
+            <img class="img-responsive" width="250" height="250" src="https://drogo-london.com/wp-content/uploads/2017/12/Unknown.png">
+          </center>
+
+        </div> </a>
+        </div>
+
+<div class="col-md-4">
+          <a href="https://drogo-london.com/shop/INFLUENCE">
+
+          <center> <div class="containerBox">
+    <div class="text-box-mobile">
+        <h4></h4>
+    </div>
+    <img class="img-responsive" width="250" height="250" src="https://drogo-london.com/wp-content/uploads/2017/12/Unknown-2.png"> </center>
+
+  </div> </a>
+        </div>
+
+
+        <div class="col-md-4">
+          <a href="https://drogo-london.com/shop/ENDURE">
+
+          <center> <div class="containerBox">
+    <div class="text-box-mobile">
+        <h4></h4>
+    </div>
+    <img class="img-responsive" width="250" height="250" src="https://drogo-london.com/wp-content/uploads/2017/12/Unknown-1.png"> </center>
+
+  </div> </a>
+        </div>
+
+<div class="blogmobile"
+         <div class="col-md-4">
+          <a href="https://drogo-london.com/shop/OUD">
+
+          <center> <div class="containerBox">
+    <div class="text-box-mobile">
+        <h4></h4>
+    </div>
+    <img class="img-responsive" width="250" height="250" src="https://drogo-london.com/wp-content/uploads/2017/12/Unknown-3.png"> </center>
+
+  </div> </a>
+        </div>
+        </div>
+
+
+</div>
+</div>
+
+
+
+
+
+&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;
